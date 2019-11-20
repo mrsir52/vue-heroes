c8368cd6553a8119e8a78ace76c03dee2dffe3a9
Manny <coachmannytw@gmail.com>
2019-11-19 20:58:14 -0700

    first vue commit

--- 03-data-and-events/begin/vue-heroes/src/components/heroes.vue
+++ 03-data-and-events/begin/vue-heroes/src/components/heroes.vue
@@ -7,12 +7,12 @@
       <div class="column is-8">
         <div class="card edit-detail">
           <header class="card-header">
-            <p class="card-header-title"></p>
+            <p class="card-header-title">{{hero.firstName}}</p>
           </header>
           <div class="card-content">
             <div class="content">
               <div class="field">
-                <label class="label" for="id">id</label>
+                <label class="label" for="id">{{hero.id}} </label>
                 <label class="input" id="id" readonly></label>
               </div>
               <div class="field">
@@ -66,17 +66,17 @@
             </div>
           </div>
           <footer class="card-footer">
-            <button class="link card-footer-item cancel-button">
+            <button class="link card-footer-item cancel-button" @click="cancelHero">
               <i class="fas fa-undo"></i>
               <span>Cancel</span>
             </button>
-            <button class="link card-footer-item">
+            <button class="link card-footer-item" @click="saveHero">
               <i class="fas fa-save"></i>
               <span>Save</span>
             </button>
           </footer>
         </div>
-        <div class="notification is-info"></div>
+        <div class="notification is-info"><pre>{{ message }}</pre></div>
       </div>
     </div>
   </div>
@@ -85,5 +85,28 @@
 <script>
 export default {
   name: 'Heroes',
+  data() {
+    return {
+      hero: {
+      id: 20,
+      firstName: 'Joe',
+      lastName: 'Mama',
+      description: 'cat sniffer',
+      capeColor: '',
+      power: '',
+      active: true,
+    },
+    message: '',
+    };
+  },
+    methods: {
+      cancelHero() {
+        this.message = '';
+      },
+      saveHero() {
+        this.message = JSON.stringify(this.hero, null, '\n');
+      },
+    },
+  
 };
 </script>
